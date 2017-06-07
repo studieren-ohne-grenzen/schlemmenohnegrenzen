ad59cf51e0f0484c62384d55acfba726929c6573
Dennis Keck <git@spam.denniskeck.de>
2017-06-07 19:43:47 +0200

    Added Mühlburg

--- frontend/choices.py
+++ frontend/choices.py
@@ -5,6 +5,7 @@ plz_choices = (
     (76137, "76137 (Südstadt, Südweststadt)"),
     (76139, "76139 (Hagsfeld, Waldstadt)"),
     (76149, "76149 (Nordstadt)"),
+    (76185, "76185 (Mühlburg)"),
     (76187, "76187 (Nordweststadt)"),
     (76227, "76227 (Durlach)")
 )
