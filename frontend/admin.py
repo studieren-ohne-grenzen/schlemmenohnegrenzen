cc8efbffadfccfc4e2f1656a6469a515c0e6314e
Daniel Renninghoff <daniel@danielrenninghoff.com>
2017-06-12 08:39:00 +0200

    Fixed database editing

--- frontend/admin.py
+++ frontend/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from frontend.models import Household, Cluster, VisitingGroup
 
-admin.site.register(Household)
 admin.site.register(Cluster)
 admin.site.register(VisitingGroup)
+
+class HouseholdAdmin(admin.ModelAdmin):
+    list_display = ('name1', 'name2', 'signup_date')
+
+admin.site.register(Household, HouseholdAdmin)
