77df769782cebdbc29638971af6de3f8e89c0539
Dennis Keck <26092524+fellhorn@users.noreply.github.com>
2018-04-24 12:45:59 +0200

    Change location of map to bonn

--- frontend/static/frontend/js/clustering.js
+++ frontend/static/frontend/js/clustering.js
@@ -8,7 +8,7 @@ function getRandomColor () {
 }
 
 function drawMap (jsonstr) {
-  var mymap = L.map('mapid').setView([49.012588, 8.403000], 13)
+  var mymap = L.map('mapid').setView([50.7329, 7.1064], 13)
   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
     maxZoom: 18
